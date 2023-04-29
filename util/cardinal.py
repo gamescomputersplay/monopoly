f126f58dbf733963b70fa061bd5810267efa3a9e
Viktor velev <viktorvelev8@gmail.com>
2023-04-29 05:22:56 +0200

    cleanup

--- util/cardinal.py
+++
@@ -1,6 +0,0 @@
-# Partially copied from https://mail.python.org/pipermail/python-list/2000-September/030907.html
-def cardinal(num):
-    return ["One", "Two", "Three", "Four", "Five", "Six", "Seven",
-              "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen",
-              "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
-              "Nineteen"][num - 1]
