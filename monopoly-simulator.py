55654683da9942adf52720e443502d4898eb0a58
gamescomputersplay <gamescomputersplay@gmail.com>
2023-10-18 14:36:50 +0800

    small chenges for multi-processing

--- monopoly-simulator.py
+++ monopoly-simulator.py
@@ -20,9 +20,11 @@ def one_game(data_for_simulation):
     For convenience to set up a multi-thread, all data packed into a tuple:
     - game number (to dicplay it in the log)
     - seed to intialize the log
-    - log handle
     '''
-    game_number, game_seed, log = data_for_simulation
+    game_number, game_seed = data_for_simulation
+
+    # Initialize log
+    log = Log()
 
     log.add(f"\n\n= GAME {game_number} of {SimulationSettings.n_simulations} (seed = {game_seed}) =")
 
@@ -41,16 +43,16 @@ def one_game(data_for_simulation):
     # Initiate the board
     board = Board(GameSettings)
 
-    # Set up dice (create a separate rnadom generator with specific seed,
+    # Set up dice (create a separate random generator with initial "game_seed",
     # to make it thread-safe)
     dice = Dice(game_seed, GameSettings.dice_count, GameSettings.dice_sides)
 
+    # Play for the required number of turns
     for turn_n in range(1, SimulationSettings.n_moves + 1):
 
-        # Start a turn
-        log.add(f"\n== Turn {turn_n} ===")
-
+        # Log a start a turn
         # Log all the players and their current position/money
+        log.add(f"\n== Turn {turn_n} ===")
         for player_n, player in enumerate(players):
             log.add(f"- Player {player_n}, '{player.name}': " +
                     f"${player.money}, at {player.position} ({board.b[player.position].name})")
@@ -59,6 +61,7 @@ def one_game(data_for_simulation):
         for player in players:
             player.make_a_move(board, players, dice, log)
 
+    # Save the log
     log.save()
     time.sleep(.2)
 
@@ -78,19 +81,15 @@ def run_simulation(config):
     if config.seed is not None:
         random.seed(config.seed)
 
-    # Initiate
-    log = Log()
-
-    # Pre-generate seeds (to have simulation multi-thread safe)
+    # Pre-generate game seeds (to have simulation multi-thread safe)
     data_for_simulation = [
-        (i + 1, random.random(), log)
+        (i + 1, random.random())
         for i in range(config.n_simulations)]
 
-    # Simulate each game with multithreading
+    # Simulate each game with multi-processing
     with concurrent.futures.ProcessPoolExecutor(max_workers=config.multi_process) as executor:
         list(tqdm(executor.map(one_game, data_for_simulation), total=len(data_for_simulation)))
 
 
 if __name__ == "__main__":
-
     run_simulation(SimulationSettings)
