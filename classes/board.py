d7f21ea7c40c5eb28602dc3e762703592274291c
gamescomputersplay <gamescomputersplay@gmail.com>
2023-10-19 11:58:15 +0800

    Error: mortgage should not undo a monopoly

--- classes/board.py
+++ classes/board.py
@@ -10,7 +10,8 @@ class Cell:
 
 
 class Property(Cell):
-    """Property Class (for Properties, Rails, Utilities)"""
+    ''' Property Class (for Properties, Rails, Utilities)
+    '''
 
     def __init__(self, name, cost_base, rent_base, cost_house, rent_house, group):
         super().__init__(name)
@@ -26,6 +27,10 @@ class Property(Cell):
 
     def __str__(self):
         return self.name
+    
+    def calculate_rent(self):
+        ''' Calculate the rent amount for this property, including monopoly, houses etc
+        '''
 class Board:
     ''' Class collecting board repalted information:
     properties and their owners, build houses, etc
@@ -42,86 +47,86 @@ class Board:
         # 0-4
         self.b.append(Cell("GO"))
         self.b.append(Property(
-            "A1 Mediterraneal Avenue", 60, 2, 50, (10, 30, 90, 160, 250), "brown"))
+            "A1 Mediterraneal Avenue", 60, 2, 50, (10, 30, 90, 160, 250), "Brown"))
         self.b.append(Cell("COM1 Community Chest"))
         self.b.append(Property(
-            "A2 Baltic Avenue", 60, 4, 50, (20, 60, 180, 320, 450), "brown"))
+            "A2 Baltic Avenue", 60, 4, 50, (20, 60, 180, 320, 450), "Brown"))
         self.b.append(Cell("PT Property Tax"))
 
         # 5-9
         self.b.append(Property(
-            "R1 Reading railroad", 200, 0, 0, (0, 0, 0, 0, 0), "rail"))
+            "R1 Reading railroad", 200, 0, 0, (0, 0, 0, 0, 0), "Railroads"))
         self.b.append(Property(
-            "B1 Oriental Avenue", 100, 6, 50, (30, 90, 270, 400, 550), "lightblue"))
+            "B1 Oriental Avenue", 100, 6, 50, (30, 90, 270, 400, 550), "Lightblue"))
         self.b.append(Cell("CH1 Chance"))
         self.b.append(Property(
-            "B2 Vermont Avenue", 100, 6, 50, (30, 90, 270, 400, 550), "lightblue"))
+            "B2 Vermont Avenue", 100, 6, 50, (30, 90, 270, 400, 550), "Lightblue"))
         self.b.append(Property(
-            "B3 Connecticut Avenue",120,8,50,(40, 100, 300, 450, 600),"lightblue"))
+            "B3 Connecticut Avenue",120,8,50,(40, 100, 300, 450, 600),"Lightblue"))
 
         # 10-14
         self.b.append(Cell("PR Prison"))
         self.b.append(Property(
-            "C1 St.Charle's Place", 140, 10, 100, (50, 150, 450, 625, 750), "pink"))
+            "C1 St.Charle's Place", 140, 10, 100, (50, 150, 450, 625, 750), "Pink"))
         self.b.append(Property(
-            "U1 Electric Company", 150, 0, 0, (0, 0, 0, 0, 0), "util"))
+            "U1 Electric Company", 150, 0, 0, (0, 0, 0, 0, 0), "Utilities"))
         self.b.append(Property(
-            "C2 States Avenue", 140, 10, 100, (50, 150, 450, 625, 750), "pink"))
+            "C2 States Avenue", 140, 10, 100, (50, 150, 450, 625, 750), "Pink"))
         self.b.append(Property(
-            "C3 Virginia Avenue", 160, 12, 100, (60, 180, 500, 700, 900), "pink"))
+            "C3 Virginia Avenue", 160, 12, 100, (60, 180, 500, 700, 900), "Pink"))
 
         # 15-19
         self.b.append(Property(
-            "R2 Pennsylvania Railroad", 200, 0, 0, (0, 0, 0, 0, 0), "rail"))
+            "R2 Pennsylvania Railroad", 200, 0, 0, (0, 0, 0, 0, 0), "Railroads"))
         self.b.append(Property(
-            "D1 St.James Place", 180, 14, 100, (70, 200, 550, 700, 950), "orange"))
+            "D1 St.James Place", 180, 14, 100, (70, 200, 550, 700, 950), "Orange"))
         self.b.append(Cell("COM2 Community Chest"))
         self.b.append(Property(
-            "D2 Tennessee Avenue", 180, 14, 100, (70, 200, 550, 700, 950), "orange"))
+            "D2 Tennessee Avenue", 180, 14, 100, (70, 200, 550, 700, 950), "Orange"))
         self.b.append(Property(
-            "D3 New York Avenue", 200, 16, 100, (80, 220, 600, 800, 1000), "orange"))
+            "D3 New York Avenue", 200, 16, 100, (80, 220, 600, 800, 1000), "Orange"))
 
         # 20-24
         self.b.append(Cell("FP Free Parking"))
         self.b.append(Property(
-            "E1 Kentucky Avenue", 220, 18, 150, (90, 250, 700, 875, 1050), "red"))
+            "E1 Kentucky Avenue", 220, 18, 150, (90, 250, 700, 875, 1050), "Red"))
         self.b.append(Cell("CH2 Chance"))
         self.b.append(Property(
-            "E2 Indiana Avenue", 220, 18, 150, (90, 250, 700, 875, 1050), "red"))
+            "E2 Indiana Avenue", 220, 18, 150, (90, 250, 700, 875, 1050), "Red"))
         self.b.append(Property(
-            "E3 Illinois Avenue", 240, 20, 150, (100, 300, 750, 925, 1100), "red"))
+            "E3 Illinois Avenue", 240, 20, 150, (100, 300, 750, 925, 1100), "Red"))
 
         # 25-29
         self.b.append(Property(
-            "R3 BnO Railroad", 200, 0, 0, (0, 0, 0, 0, 0), "rail"))
+            "R3 BnO Railroad", 200, 0, 0, (0, 0, 0, 0, 0), "Railroads"))
         self.b.append(Property(
-            "F1 Atlantic Avenue", 260, 22, 150, (110, 330, 800, 975, 1150), "yellow"))
+            "F1 Atlantic Avenue", 260, 22, 150, (110, 330, 800, 975, 1150), "Yellow"))
         self.b.append(Property(
-            "F2 Ventinor Avenue", 260, 22, 150, (110, 330, 800, 975, 1150), "yellow"))
+            "F2 Ventinor Avenue", 260, 22, 150, (110, 330, 800, 975, 1150), "Yellow"))
         self.b.append(Property(
-            "U2 Waterworks", 150, 0, 0, (0, 0, 0, 0, 0), "util"))
+            "U2 Waterworks", 150, 0, 0, (0, 0, 0, 0, 0), "Utilities"))
         self.b.append(Property(
-            "F3 Martin Gardens", 280, 24, 150, (120, 360, 850, 1025, 1200), "yellow"))
+            "F3 Martin Gardens", 280, 24, 150, (120, 360, 850, 1025, 1200), "Yellow"))
 
         # 30-34
         self.b.append(Cell("GTJ Go To Jail"))
         self.b.append(Property(
-            "G1 Pacific Avenue", 300, 26, 200, (130, 390, 900, 1100, 1275), "green"))
+            "G1 Pacific Avenue", 300, 26, 200, (130, 390, 900, 1100, 1275), "Green"))
         self.b.append(Property(
-            "G2 North Carolina Avenue", 300, 26, 200, (130, 390, 900, 1100, 1275), "green"))
+            "G2 North Carolina Avenue", 300, 26, 200, (130, 390, 900, 1100, 1275), "Green"))
         self.b.append(Cell("COM3 Community Chest"))
         self.b.append(Property(
-            "G3 Pennsylvania Avenue", 320, 28, 200, (150, 450, 100, 1200, 1400), "green"))
+            "G3 Pennsylvania Avenue", 320, 28, 200, (150, 450, 100, 1200, 1400), "Green"))
 
         # 35-39
         self.b.append(Property(
-            "R4 Short Line", 200, 0, 0, (0, 0, 0, 0, 0), "rail"))
+            "R4 Short Line", 200, 0, 0, (0, 0, 0, 0, 0), "Railroads"))
         self.b.append(Cell("CH3 Chance"))
         self.b.append(Property(
-            "H1 Park Place", 350, 35, 200, (175, 500, 1100, 1300, 1500), "indigo"))
+            "H1 Park Place", 350, 35, 200, (175, 500, 1100, 1300, 1500), "Indigo"))
         self.b.append(Cell("LT Luxury Tax"))
         self.b.append(Property(
-            "H2 Boardwalk", 400, 50, 200, (200, 600, 1400, 1700, 2000), "indigo"))
+            "H2 Boardwalk", 400, 50, 200, (200, 600, 1400, 1700, 2000), "Indigo"))
 
     def check_if_monopoly(self, property_to_check):
         ''' Checks if property_to_check is a monopoly. If it is (or isn't),
@@ -131,15 +136,11 @@ class Board:
         # This is the group we'll be looking at
         group_to_check = property_to_check.group
 
-        # Collect all owners of this group in this set. For this
-        # particular check, consider mortgaged property to have no owner
+        # Collect all owners of this group in this set. 
         owners = set()
         for cell in self.b:
             if isinstance(cell, Property) and cell.group == group_to_check:
-                if cell.is_mortgaged:
-                    owners.add(None)
-                else:
-                    owners.add(cell.owner)
+                owners.add(cell.owner)
 
         # It is a monopoly: update the monopoly status
         if len(owners) == 1 and None not in owners:
