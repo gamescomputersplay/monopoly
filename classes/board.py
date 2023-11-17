1e822f7a6f1b56fd8355180c9a77115c7e6102cf
gamescomputersplay <gamescomputersplay@gmail.com>
2023-11-17 18:24:21 +0800

    Actual Chest and Chance cards

--- classes/board.py
+++ classes/board.py
@@ -216,18 +216,42 @@ class Board:
 
         # Chance deck
         self.chance = Deck([
-            "Chance 1",
-            "Chance 2",
-            "Chance 3",
-            "Chance 4"
+            "Advance to Boardwalk",
+            "Advance to Go (Collect $200)",
+            "Advance to Illinois Avenue. If you pass Go, collect $200",
+            "Advance to St. Charles Place. If you pass Go, collect $200",
+            "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled",
+            "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled",
+            "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown.",
+            "Bank pays you dividend of $50",
+            "Get Out of Jail Free",
+            "Go Back 3 Spaces",
+            "Go to Jail. Go directly to Jail, do not pass Go, do not collect $200",
+            "Make general repairs on all your property. For each house pay $25. For each hotel pay $100",
+            "Speeding fine $15",
+            "Take a trip to Reading Railroad. If you pass Go, collect $200",
+            "You have been elected Chairman of the Board. Pay each player $50",
+            "Your building loan matures. Collect $150"
         ])
 
         # Community Chest deck
         self.chest = Deck([
-            "Community Chest 1",
-            "Community Chest 2",
-            "Community Chest 3",
-            "Community Chest 4"
+            "Advance to Go (Collect $200)",
+            "Bank error in your favor. Collect $200",
+            "Doctor's fee. Pay $50",
+            "From sale of stock you get $50",
+            "Get Out of Jail Free",
+            "Go to Jail. Go directly to jail, do not pass Go, do not collect $200",
+            "Holiday fund matures. Receive $100",
+            "Income tax refund. Collect $20",
+            "It is your birthday. Collect $10 from every player",
+            "Life insurance matures. Collect $100",
+            "Pay hospital fees of $100",
+            "Pay school fees of $50",
+            "Receive $25 consultancy fee",
+            "You are assessed for street repair. $40 per house. $115 per hotel",
+            "You have won second prize in a beauty contest. Collect $10",
+            "You inherit $100"
         ])
 
     def property_groups(self):
