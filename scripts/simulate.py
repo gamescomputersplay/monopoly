52b402388f625382475c3ed5d35aca123529fdcd
Gal Cohensius <gal@bbumgames.com>
2025-05-01 21:18:17 +0300

    master_rng that gets the seed from SimulationSettings

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -16,19 +16,12 @@ def run_simulation(config):
     bankruptcies_log = Log(LogSettings.BANKRUPTCIES_PATH)
     bankruptcies_log.reset("game_number\tplayer_bankrupt\tturn")
 
-    # Initiate an overall random generator with the seed from the settings.py file
-    if config.seed is not None:
-        random.seed(config.seed)
+    master_rng = random.Random(config.seed)
+    data_for_simulation = [(i + 1, master_rng.getrandbits(32)) for i in range(config.n_games)]
 
-    # With that overall random generator, pre-generate game seeds
-    # `data_for_simulation` is a list of tuples: (game_number, game_seed)
-    data_for_simulation = [(i + 1, random.randint(0, 2 ** 32 - 1)) for i in range(config.n_games)]
-
-    # Simulate each game with multiprocessing
     with concurrent.futures.ProcessPoolExecutor(max_workers=config.multi_process) as executor:
         list(tqdm(executor.map(monopoly_game, data_for_simulation), total=len(data_for_simulation)))
 
-    # Print analysis of the simulation (data is read from the bankruptcies_log file)
     analyzer = Analyzer()
     analyzer.run_all()
 
