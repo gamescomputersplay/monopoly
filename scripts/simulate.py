f1abdb63522061765f788e6c218d90dcf5209b34
Gal Cohensius <gal@bbumgames.com>
2025-05-06 12:08:26 +0300

    Simplify concurrent runs: swap executor.map+bar for tqdm.process_map

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -1,14 +1,12 @@
 import random
-import concurrent.futures
 from typing import Type
 
-from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
-from monopoly.core.game import monopoly_game
-from settings import SimulationSettings
-from monopoly.log_settings import LogSettings
 from monopoly.analytics import Analyzer
+from monopoly.core.game import monopoly_game
+from monopoly.log_settings import LogSettings
+from settings import SimulationSettings
 
 
 def run_simulation(config: Type[SimulationSettings]) -> None:
